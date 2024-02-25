07ac8b05e5b4f0151b4b8d70a04a979f2b241e83
Palat7 <152816239+Palat7@users.noreply.github.com>
2024-02-25 14:57:11 -0500

    Les controleurs de la prise de rendez-vous

---
+++ controleurs/get_disponibilites_medecin.php
@@ -0,0 +1,30 @@
+<?php
+// Connexion à la base de données
+$nom_utilisateur = "root";
+$mot_passe = "root";
+$configuration = "mysql:host=localhost;dbname=mydb";
+$connexionBD = new PDO($configuration, $nom_utilisateur, $mot_passe);
+
+//Récupération de la valeur du médecin sélectionné depuis la requête GET
+$medecin = isset($_GET['listeMedecins']) ? $_GET['listeMedecins'] : '';
+
+//Requête SQL pour récupérer les disponibilités du médecin sélectionné
+$sql = "SELECT md.idDisponibilite, md.MD_idMedecin, m.Medecin_idClinique, md.MD_idPlageHoraire, ph.jourSemaines, ph.heureDebut, 
+       ph.heureFin, md.statutDispo, md.MD_idServices FROM medecin_disponibilite AS md 
+        INNER JOIN medecin AS m ON md.MD_idMedecin = m.idMedecin 
+        INNER JOIN plagehoraire AS ph ON md.MD_idPlageHoraire = ph.idPlageHoraire 
+        INNER JOIN clinique AS cl ON m.Medecin_idClinique = cl.idClinique
+        WHERE md.MD_idMedecin = ?";
+$stmt = $connexionBD->prepare($sql);
+
+if ($stmt) {
+    $stmt->execute(array($medecin));
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Renvoi des disponibilités au format JSON
+    echo json_encode($result);
+} else {
+
+    echo json_encode(array('error' => 'Erreur lors de la préparation de la requête'));
+}
+?>
---
+++ controleurs/traitement_rendezvous.php
@@ -0,0 +1,49 @@
+<?php
+// Connexion à la base de données
+global $sql;
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "mydb";
+
+//Création la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Vérification de la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
+
+//Récupération des données du formulaire
+$raisonRV = $_POST['raisonRV'];
+$description = $_POST['description'];
+$idDisponibilite = $_POST['idDisponibilite'];
+$idMedecin = $_POST['idMedecin'];
+$idClinique = $_POST['idClinique'];
+$jourSemaine = $_POST['jourSemaine'];
+
+//Devra être récupéré à partir de la session, modification à faire
+//$idPatient = $_POST['idPatient'];
+
+$idPatient = 1;
+// Préparation et exécution de la requête d'insertion
+$sql = "INSERT INTO rendezvous (dateRDV, raisonRdv, detailsRdv, RDV_idPatient, RDV_idMedecin, RDV_idClinique, RDV_idDisponibilite) 
+        VALUES ('$jourSemaine', '$raisonRV', '$description', '$idPatient' , '$idMedecin', '$idClinique', '$idDisponibilite')";
+
+
+if ($conn->query($sql) === TRUE) {
+// Mise à jour du champ statutDispo de la disponibilité réservée
+    $sqlUpdateDispo = "UPDATE medecin_disponibilite SET statutDispo = 'NON' WHERE idDisponibilite = '$idDisponibilite'";
+    if ($conn->query($sqlUpdateDispo) === TRUE) {
+
+        echo "Rendez-vous pris avec succès !";
+
+
+    }
+} else {
+    echo "Erreur lors de la prise de rendez-vous : " . $conn->error;
+}
+
+// Fermeture de la connexion à la base de données
+$conn->close();
+?>
